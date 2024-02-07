415fc94ac08375ecfce18ba792300c9c8a1094a8
dwnsnu <dwnsnu@snu.ac.kr>
2024-02-07 15:43:53 +0900

    update

--- kube-freesurfer/freesurfer-parallel.sh
+++ kube-freesurfer/freesurfer-parallel.sh
@@ -2,7 +2,7 @@
 
 for i in {900..910}; do
   # Job 파일 이름 정의
-  job_file="freesurfer-job-template.yaml"
+  job_file="freesurfer-job-template.yml"
 
   # 템플릿 파일에서 ${INDEX}를 실제 인덱스 값으로 대체하여 job_file 생성
   sed "s/\${INDEX}/$i/g" freesurfer-job-template.yaml > $job_file
