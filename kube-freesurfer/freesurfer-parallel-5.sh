47e930b530349958db88cc9d8f687963941a0523
dwnsnu <dwnsnu@snu.ac.kr>
2024-02-07 17:36:35 +0900

    update: freesurfer-job-template syntax error fixed

--- kube-freesurfer/freesurfer-parallel-5.sh
+++ kube-freesurfer/freesurfer-parallel-5.sh
@@ -14,6 +14,6 @@ original_file="freesurfer-job-template.yml"
 new_file="freesurfer-job-${lbl}-${idx1}.yml"
 
 # sed를 사용하여 변수 대체하고 결과를 새 파일에 저장
-sed "s/\${LBL}/$lbl/g; s/\${USR}/$usr/g; s/\${IDX1}/$idx1/g; s/\${IDX2}/$idx2/g; s/\${IDX3}/$idx3/g; s/\${IDX4}/$idx4/g; s/\${IDX5}/$idx5/g; " $original_file > $new_file
+sed "s/\${LBL}/$lbl/g; s/\${USR}/$usr/g; s/\${IDX1}/$idx1/g; s/\${IDX2}/$idx2/g; s/\${IDX3}/$idx3/g; s/\${IDX4}/$idx4/g; s/\${IDX5}/$idx5/g" $original_file > $new_file
 
 kubectl apply -f $new_file
