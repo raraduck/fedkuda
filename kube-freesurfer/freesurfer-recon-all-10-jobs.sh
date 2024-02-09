40d7476f80461683090b81e6d82470746a09f745
dwnusa-win10 <dwnusa@gmail.com>
2024-02-09 23:05:55 +0900

    recon-all-10

--- kube-freesurfer/freesurfer-recon-all-10-jobs.sh
+++ kube-freesurfer/freesurfer-recon-all-10-jobs.sh
@@ -32,7 +32,7 @@ original_file="freesurfer-recon-all-10-jobs.yml"
 new_file="$logs_folder/freesurfer-recon-all-jobs-${lbl}-${idx1}-${idx2}-${idx3}-${idx4}-${idx5}-${idx6}-${idx7}-${idx8}-${idx9}-${idx10}.yml"
 
 # sed를 사용하여 변수 대체하고 결과를 새 파일에 저장
-sed "s/\${LBL}/$lbl/g; s/\${USR}/$usr/g; s/\${IDX1}/$idx1/g; s/\${IDX2}/$idx2/g; s/\${IDX3}/$idx3/g; s/\${IDX4}/$idx4/g; s/\${IDX5}/$idx5/g; s/\${IDX6}/$idx6/g; s/\${IDX7}/$idx7/g; s/\${IDX8}/$idx8/g s/\${IDX9}/$idx9/g; s/\${IDX10}/${idx10}/g" $original_file > $new_file
+sed "s/\${LBL}/$lbl/g; s/\${USR}/$usr/g; s/\${IDX1}/$idx1/g; s/\${IDX2}/$idx2/g; s/\${IDX3}/$idx3/g; s/\${IDX4}/$idx4/g; s/\${IDX5}/$idx5/g; s/\${IDX6}/$idx6/g; s/\${IDX7}/$idx7/g; s/\${IDX8}/$idx8/g; s/\${IDX9}/$idx9/g; s/\${IDX10}/$idx10/g" $original_file > $new_file
 
 kubectl apply -f $new_file
 
